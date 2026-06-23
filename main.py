a36ebb1697fca106944cae65a9f79214043114af
Muhammadbilol Qo'chqorov <mblabx@gmail.com>
2026-06-23 19:24:49 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "@Pythonuz09 dan stiker")
 arcade.set_background_color(arcade.color.WHITE)
 arcade.start_render()
 x = 300
-y = 300
+y = 300qswdfegthy
 radius = 200
 arcade.draw_circle_filled(x, y, radius, arcade.color.YELLOW)
 x = 370
