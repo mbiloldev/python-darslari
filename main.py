82154591122ce5b6583d7c78691e496f6d183113
Muhammadbilol Qo'chqorov <mblabx@gmail.com>
2026-06-23 19:25:00 +0500

    Fix variable assignment and cleanup code

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "@Pythonuz09 dan stiker")
 arcade.set_background_color(arcade.color.WHITE)
 arcade.start_render()
 x = 300
-y = 300qswdfegthy
+y = 300
 radius = 200
 arcade.draw_circle_filled(x, y, radius, arcade.color.YELLOW)
 x = 370
