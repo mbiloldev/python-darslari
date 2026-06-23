bfefb12ba41cbae6e9046047c6a3d53cbac2a684
Muhammadbilol Qo'chqorov <mblabx@gmail.com>
2026-06-23 19:16:55 +0500

    Implement turtle graphics art drawing function
    Added a turtle graphics function to draw colorful art.

--- a.py
+++ a.py
@@ -1,1 +1,20 @@
-ewfghtmj,kyl
+import turtle
+
+
+def draw_art():
+    window = turtle.Screen()
+    window.bgcolor("black")
+
+    artist = turtle.Turtle()
+    artist.speed(0)
+    colors = ["red", "yellow", "blue", "green"]
+
+    for i in range(180):
+        artist.color(colors[i % 4])
+        artist.forward(i * 2)
+        artist.left(90)
+
+    window.mainloop()
+
+
+draw_art()
