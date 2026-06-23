7d9e29ad92ec9ef9e35d6c105b2dd1b63f249cbd
Muhammadbilol Qo'chqorov <mblabx@gmail.com>
2026-06-23 19:37:32 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import arcade
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
 arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "@Pythonuz09 dan stiker")
-arcade.set_background_color(arcade.color.WHITE)
+arcade.set_background_color(arcade.color.WHITE)hyjhm
 arcade.start_render()
 x = 300
 y = 300
